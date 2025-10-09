5087ee46beef37279752762f19b31cbbd2531c8b
Manuel <Manusolerj@gmail.com>
2025-10-09 12:29:14 +0200

    Initialize Maven project with wrapper scripts, configuration files, and basic application structure

---
+++ src/test/java/com/pokemon/project/teambuilder/TeambuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.project.teambuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeambuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
