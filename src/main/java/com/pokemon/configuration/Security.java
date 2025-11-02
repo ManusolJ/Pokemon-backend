f0643d462c094d518eeb80df67d79d8c28f7504e
Manu <manusolerj@gmail.com>
2025-11-02 17:40:37 +0100

    Feat: Add Security configuration with BCryptPasswordEncoder for password encryption

---
+++ src/main/java/com/pokemon/configuration/Security.java
@@ -0,0 +1,13 @@
+package com.pokemon.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class Security {
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
