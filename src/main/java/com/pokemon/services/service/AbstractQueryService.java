5f155816cdf48f7565db6b154eccb123d7997c52
Manu <manusolerj@gmail.com>
2025-11-20 15:34:52 +0100

    Refactor: Remove unnecessary @Service annotation and ensure read-only transactions in AbstractQueryService

--- src/main/java/com/pokemon/services/service/AbstractQueryService.java
+++ src/main/java/com/pokemon/services/service/AbstractQueryService.java
@@ -1,10 +1,12 @@
 package com.pokemon.services.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -13,8 +15,8 @@ import com.pokemon.utils.mappers.BaseMapper;
 
 import lombok.RequiredArgsConstructor;
 
-@Service
 @Validated
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public abstract class AbstractQueryService<E, ID, R extends BaseRepository<E, ID>, D, M extends BaseMapper<E, D>>
         implements BaseQueryService<D, ID> {
@@ -34,6 +36,12 @@ public abstract class AbstractQueryService<E, ID, R extends BaseRepository<E, ID
     public Page<D> findAll(@NonNull Pageable pageable) {
         return repository.findAll(pageable)
                 .map(mapper::toDto);
+    }
+
+    public List<D> findAll() {
+        return repository.findAll().stream()
+                .map(mapper::toDto)
+                .toList();
     }
 
     public boolean existsById(@NonNull ID id) {
