8204f3bab38af4bb34e440f43971186dbe290970
Manu <manusolerj@gmail.com>
2025-11-20 14:13:07 +0100

    Refactor: Simplify UserMapper by extending BaseMapper and removing redundant methods

--- src/main/java/com/pokemon/utils/mappers/BaseMapper.java
+++ src/main/java/com/pokemon/utils/mappers/BaseMapper.java
@@ -2,7 +2,4 @@ package com.pokemon.utils.mappers;
 
 public interface BaseMapper<E, D> {
 
-    D toDto(E entity);
-
-    E toEntity(D dto);
 }
--- src/main/java/com/pokemon/utils/mappers/UserMapper.java
+++ src/main/java/com/pokemon/utils/mappers/UserMapper.java
@@ -1,33 +1,22 @@
 package com.pokemon.utils.mappers;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
 import com.pokemon.dtos.user.CreateUserDto;
 import com.pokemon.dtos.user.ReadUserDto;
 import com.pokemon.dtos.user.UpdateUserDto;
 import com.pokemon.entities.User;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper {
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper extends BaseMapper<User, ReadUserDto> {
 
-    ReadUserDto toReadUserDto(User user);
+    ReadUserDto toDto(User entity);
 
-    List<ReadUserDto> toReadUserDtoList(List<User> users);
+    @Mapping(source = "password", target = "passwordHash")
+    User toEntity(CreateUserDto dto);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "passwordHash", ignore = true)
-    @Mapping(target = "role", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    User toEntity(CreateUserDto createUserDto);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "passwordHash", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    void updateEntityFromDto(UpdateUserDto updateUserDto, @MappingTarget User user);
+    void updateFromDto(UpdateUserDto dto, @MappingTarget User entity);
 }
