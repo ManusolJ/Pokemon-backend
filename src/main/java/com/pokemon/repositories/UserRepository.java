32cd3e7531a7f410d68673f5684b104ed7ef8f61
Manu <manusolerj@gmail.com>
2025-11-20 14:19:23 +0100

    Feat: Add method to update active status of users by their IDs in UserRepository

--- src/main/java/com/pokemon/repositories/UserRepository.java
+++ src/main/java/com/pokemon/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.pokemon.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pokemon.entities.User;
@@ -8,4 +10,8 @@ import com.pokemon.entities.User;
 public interface UserRepository extends BaseRepository<User, Long> {
 
     boolean existsByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE User u SET u.active = :active WHERE u.id IN :ids")
+    int updateActiveStatusByIds(boolean active, Iterable<Long> ids);
 }
