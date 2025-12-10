09318f3ba51eef31b8d9f2126e11141cd5427938
Manu <manusolerj@gmail.com>
2025-12-10 11:00:49 +0100

    feat: Enhance RestClientConfiguration with request factory and logging interceptor

--- src/main/java/com/pokemon/configuration/RestClientConfiguration.java
+++ src/main/java/com/pokemon/configuration/RestClientConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class RestClientConfiguration {
 
     @Bean
@@ -14,6 +18,11 @@ public class RestClientConfiguration {
         return RestClient.builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .requestInterceptor((request, body, execution) -> {
+                    log.debug("Calling: {} {}", request.getMethod(), request.getURI());
+                    return execution.execute(request, body);
+                })
                 .build();
     }
 }
