b9b6304994c2276f8360ef6cb3a6b3867a439aed
Manu <manusolerj@gmail.com>
2025-12-14 01:46:34 +0100

    feat: Add fromString method to DamageClass enum for string conversion

--- src/main/java/com/pokemon/utils/enums/DamageClass.java
+++ src/main/java/com/pokemon/utils/enums/DamageClass.java
@@ -3,5 +3,14 @@ package com.pokemon.utils.enums;
 public enum DamageClass {
     PHYSICAL,
     SPECIAL,
-    STATUS
+    STATUS;
+
+    public static DamageClass fromString(String value) {
+        for (DamageClass damageClass : DamageClass.values()) {
+            if (damageClass.name().equalsIgnoreCase(value)) {
+                return damageClass;
+            }
+        }
+        throw new IllegalArgumentException("Unknown DamageClass: " + value);
+    }
 }
