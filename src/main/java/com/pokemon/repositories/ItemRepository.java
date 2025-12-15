746ca05d9bcb40fd30b4289009f33120cf705332
Manu <manusolerj@gmail.com>
2025-12-15 15:48:30 +0100

    feat: Add NoRepositoryBean annotation to BaseRepository and ensure ItemRepository is annotated as a Repository

--- src/main/java/com/pokemon/repositories/ItemRepository.java
+++ src/main/java/com/pokemon/repositories/ItemRepository.java
@@ -1,7 +1,10 @@
 package com.pokemon.repositories;
 
+import org.springframework.stereotype.Repository;
+
 import com.pokemon.entities.Item;
 
+@Repository
 public interface ItemRepository extends BaseRepository<Item, Long> {
 
 }
