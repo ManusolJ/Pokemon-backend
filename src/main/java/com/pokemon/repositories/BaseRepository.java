98e83dedce529fb2c35b7b11ec1c9c3593f49ffb
Manu <manusolerj@gmail.com>
2025-12-18 17:06:51 +0100

    Chore: Enhance documentation for BaseRepository interface

--- src/main/java/com/pokemon/repositories/BaseRepository.java
+++ src/main/java/com/pokemon/repositories/BaseRepository.java
@@ -4,7 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
+/**
+ * Base repository interface providing common persistence operations for all entities.
+ * 
+ * This interface is not intended to be instantiated directly. It serves as a shared parent for
+ * concrete repository interfaces, exposing standard CRUD operations via {@link JpaRepository} and
+ * criteria-based query support via {@link JpaSpecificationExecutor}.
+ *
+ * @param <T> the type of the entity
+ * @param <ID> the type of the entity identifier
+ */
 @NoRepositoryBean
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 
 }
+
